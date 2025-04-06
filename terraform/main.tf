da6a16a75fbb87b8aee86bad12f63f05f87706d5
Jitendra Singh <129168609+JitendraSinghNegi@users.noreply.github.com>
2025-04-06 17:45:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,48 @@
-
-resource "aws_ecs_cluster" "medusa_cluster" {
-  name = "medusa-cluster"
+provider "aws" {
+  region = "ap-south-1"  # Set to your desired AWS region
 }
 
-resource "aws_ecs_task_definition" "medusa_task" {
-  family                   = "medusa-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([{
-    name      = "medusa"
-    image     = "ghcr.io/medusajs/medusa:v2.0.0"
-    essential = true
-    portMappings = [{
-      containerPort = 9000
-      protocol      = "tcp"
-    }]
-  }])
+# VPC, Subnets, and Security Groups
+resource "aws_vpc" "medusa_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_subnet" "medusa_subnet" {
+  vpc_id            = aws_vpc.medusa_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "up-south-1"
+}
+
+resource "aws_security_group" "medusa_sg" {
+  vpc_id = aws_vpc.medusa_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# IAM Role for ECS Task Execution
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
+
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow"
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
       Principal = {
         Service = "ecs-tasks.amazonaws.com"
       }
-      Action = "sts:AssumeRole"
     }]
   })
 }
@@ -41,16 +52,43 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+# ECS Cluster
+resource "aws_ecs_cluster" "medusa_cluster" {
+  name = "medusa-cluster"
+}
+
+# ECS Task Definition
+resource "aws_ecs_task_definition" "medusa_task" {
+  family                   = "medusa-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions    = jsonencode([{
+    name      = "medusa-backend"
+    image     = "mohitkharayat/my-medusa-app:latest"  
+    portMappings = [
+      {
+        containerPort = 80
+        hostPort     = 80
+      }
+    ]
+  }])
+}
+
+# ECS Service
 resource "aws_ecs_service" "medusa_service" {
   name            = "medusa-service"
-  cluster         = aws_ecs_cluster.medusa_cluster.arn
+  cluster         = aws_ecs_cluster.medusa_cluster.id
   task_definition = aws_ecs_task_definition.medusa_task.arn
-  launch_type     = "FARGATE"
   desired_count   = 1
+  launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = ["subnet-xxxxxxxx", "subnet-yyyyyyyy"]
-    security_groups = ["sg-zzzzzzzzzz"]
+    subnets          = [aws_subnet.medusa_subnet.id]
+    security_groups  = [aws_security_group.medusa_sg.id]
     assign_public_ip = true
   }
 }
